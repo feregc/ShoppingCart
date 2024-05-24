11ece7b56f34db2bdce7d891e397f8b5a3688dee
Fernando Garcia <gfernandoentique37@gmail.com>
2024-05-24 16:21:16 -0600

    creating OrderRepositoryTests

--- ShoppingCartApi.Tests/obj/Debug/net8.0/ShoppingCartApi.Tests.AssemblyInfo.cs
+++ ShoppingCartApi.Tests/obj/Debug/net8.0/ShoppingCartApi.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingCartApi.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5c6c60fb5924eb3adcc69dac4e0c44ed7cc49a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cd8bfd130c165605d056912af44e89ac3941b8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingCartApi.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingCartApi.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
